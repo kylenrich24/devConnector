9fbd6c9cb65bfd63785ee988034f1b814b8be226
Kyle <kylennardsy@hotmail.com>
2020-03-29 21:21:37 -0400

    created custom middleware to authenticate with JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/Users");
 
 // @route   GET api/auth
 // @desc    Auth route
 // @access  Public
-router.get("/", (req, res) => res.send("User route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
